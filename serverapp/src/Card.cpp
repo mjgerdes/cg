0de51b2513d2690659dc59afddb71e3bf90a9df5
marius <mgerdes@uni-potsdam.de>
2016-03-23 00:37:36 +0100

    Added some verification to card loading.

--- serverapp/src/Card.cpp
+++ serverapp/src/Card.cpp
@@ -1,7 +1,33 @@
 
+#include <iostream>
+#include <sstream>
+#include <string>
 #include "Card.hpp"
 
 bool Card::CardVerifier::operator()(const data_type* data) {
+	std::stringstream namestringstream;
+	namestringstream << " (card id: " << data->id() << ", name: " << data->name() << ")\n";
+	std::string warningstring{"Warning in Card::verifier: "};
+	std::string namestring{namestringstream.str()};
+
+	if(data->id() == data_type::universal) 
+		return true;
+
+	if(data->type() == data_type::none)
+		std::cerr << warningstring << "No SMS type set " << namestring;
+
+	if(data->name().empty())
+		std::cerr << warningstring << "No name set " << namestring;
+
+	if(data->description().empty())
+		std::cerr << warningstring << "No description set" << namestring;
+
+	if(data->fluff().empty())
+		std::cerr << warningstring << "No fluff text set, shame on you!" << namestring;
+
+	if(data->system_id() == 0)
+		std::cerr << warningstring << "No system specified" << namestring;
+
 	return true;
 }
 
