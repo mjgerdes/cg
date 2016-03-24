165daa1f29a9a457319bf35a498d7606f3d7e975
marius <mgerdes@uni-potsdam.de>
2016-03-24 09:20:26 +0100

    CardDataProvider in client javascript can now read compiled protocol buffer messages through an asynchronous (note: not asynchronous) http request. This is cool, because we can actually read binary data with jquery thanks to a binary transport plugin.
     - FIXME: The asynchronous calls are problematic because the service might be used before it has data; to fix this right now we are spin-locking on a call to the service - a proper solution would be to use angulars $http and promises but this does not work so easily with the binary mode modification, which forces us to use $.ajax instead of $.get

--- html/app/controllers/CollectionController.js
+++ html/app/controllers/CollectionController.js
@@ -1,5 +1,7 @@
 
 app.controller('CollectionController', function ($scope, $location, WebSockService, CardDataService) {
 
+$scope.test = CardDataService.get(CardDataService.types.basic_missile).description;
+
 	});
 
--- html/app/services/CardDataService.js
+++ html/app/services/CardDataService.js
@@ -1,3 +1,30 @@
-app.service('CardDataService', function ($rootScope) {
+app.service('CardDataService', function ($rootScope, $http) {
+var DataBuilder = ProtoBuf.loadProtoFile("appdata/proto/CardData.proto");
+var Data = DataBuilder.build("data");
+var wrapper = new Data.CardDataWrapper();
+var cards = {};
+var ready = false;
+
+this.types = Data.CardData.CardId;
+this.get = function (id) {
+	while(!ready) {}
+	return cards[id];
+	}
+
+
+$.ajax({
+	url : "appdata/data/cards/compile.dat",
+	type : "GET",
+	dataType : "binary",
+	responseType : "arraybuffer",
+	processResponse : false,
+	success : function (data) {
+var msg = Data.CardDataWrapper.decode(data);
+for(var i = 0; i < msg.cards.length; i++) {
+	cards[msg.cards[i].id] = msg.cards[i];
+	}
+		ready = true;
+		}});
+
 
 });
