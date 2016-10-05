65afac113386293dc40bcf44bc6329786c5e7b12
marius <mgerdes@uni-potsdam.de>
2016-10-05 11:47:44 +0200

    PlayController.js:  - Added sending of PlayInitiationRequest message on load; not tested yet!

--- html/app/controllers/PlayController.js
+++ html/app/controllers/PlayController.js
@@ -2,5 +2,10 @@
 app.controller('PlayController', ['$scope', '$location', 'WebSockService', 'CardDataService', '$cookies',
 function ($scope, $location, WebSockService, CardDataService, $cookies) {
 
+
+  // on startup we send a request to iniate play and be put in th queue
+var PlayInitiationRequestMsg = new ClientMessage({
+	"msgType" : "PlayInitiationRequestType"});
+	WebSockService.sendMsg(cardCollectionRequestMsg);
 }]);
 
