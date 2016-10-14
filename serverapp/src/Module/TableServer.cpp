8bce95608dca3327b1cfdbf24b53ffd568e35f65
marius <mgerdes@uni-potsdam.de>
2016-10-14 09:28:22 +0200

    Integration of Table class and proto messages as well as randomness into Table.hpp/cpp; some initialization of play mode also present, but incomplete.

--- serverapp/src/Module/TableServer.cpp
+++ serverapp/src/Module/TableServer.cpp
@@ -1,14 +1,105 @@
-
+#include "Table.hpp"
 #include "Module/TableServer.hpp"
 #include "Module/PlayModule.hpp"
+#include "Module/AuthModule.hpp"
+#include "Module/DataModule.hpp"
 
-TableServer::TableServer(PlayModule& parent) : m_parent(parent) {}
+#include "database.hpp"
+#include "db/Player.hpp"
+#include "Player_odb.h"
+#include "CardProvider.hpp"
+#include "SystemProvider.hpp"
+#include "HullProvider.hpp"
+#include "db/ShipPlan.hpp"
 
-void TableServer::enqueueMatch(const GameServer::ConnectionId p1,
-				  const GameServer::ConnectionId p2) {
+// constructor
+TableServer::TableServer(PlayModule& parent, AuthModule* auth, DataModule* data,
+						 GameServer::Database_type* db,
+						 GameServer::LogServer_type& ls)
+	: m_parent(parent), m_auth(auth), m_data(data), m_db(db), logServer(ls), m_rand(std::move(std::make_unique<RandomProvider>())) {
+	Table_type::setDataProvider(*m_data);
+}
+
+void TableServer::enqueueMatch(WSConnection p1, WSConnection p2) {
 	m_incomingMatches.emplace(p1, p2);
 }  // end enqueueMatch
 
 void TableServer::run() {
+	while (true) {
+		// check if we have to create a new table
+		if (!m_incomingMatches.empty()) {
+			auto ps = m_incomingMatches.pop();
+			createTable(ps.first, ps.second);
+		}
+
+		// check if anything needs to be done for the tables running
+		serveTables();
+	}  // end while
+}  // end run
+
+void TableServer::createTable(const WSConnection p1, const WSConnection p2) {
+	auto i = TablePool::create();
+	auto table = TablePool::at(i);
+	table.initialize(i);
+	// fill in cards from shipplan
+	if (!fillModel(&table, p1, p2)) {
+		return;  // arghhhg!!11
+	}
+
+	// FIXME:  send messages to players
 }
+
+bool TableServer::fillModel(Table_type* table, const WSConnection p1Connection,
+							const WSConnection p2Connection) {
+	auto p1MaybeId = m_auth->getIdFor(p1Connection);
+	auto p2MaybeId = m_auth->getIdFor(p1Connection);
+	if (!p1MaybeId || !p2MaybeId) {
+		// Player started a game without being registered
+		// FIXME: how did we get here?
+		return false;
+	}
+
+	db::Player::ShipPlanCollection_type p1ShipPlan;
+	db::Player::ShipPlanCollection_type p2ShipPlan;
+	{
+		odb::transaction t{m_db->begin()};
+		auto player1 =
+			std::unique_ptr<db::Player>{m_db->load<db::Player>(*p1MaybeId)};
+		auto player2 =
+			std::unique_ptr<db::Player>{m_db->load<db::Player>(*p2MaybeId)};
+		p1ShipPlan = player1->getShipPlans();
+		p2ShipPlan = player2->getShipPlans();
+	}  // end db
+
+	// FIXME: we always just take the first shipplan (we know its there), only
+	// for testing, add lookup of prefered plan of player here
+	// hulls
+	const auto p1Ship = p1ShipPlan[0];
+	const auto p2Ship = p2ShipPlan[0];
+	table->m_model.set_player1_hull_id(p1Ship->hull());
+	table->m_model.set_player2_hull_id(p2Ship->hull());
+
+	// systems
+
+	const auto p1Systems = p1Ship->systemPlans();
+	const auto p2Systems = p2Ship->systemPlans();
+
+	for (const auto& p1sys : p1Systems) {
+		table->m_model.add_player1_system_ids(p1sys->system());
+	}
+
+	for (const auto& p2sys : p2Systems) {
+		table->m_model.add_player2_system_ids(p2sys->system());
+	}
+
+	return true;
+}  // end fillModel
+
+void TableServer::destroyTable(TablePool::index_type i) {
+	// FIXME:  send players table destroyed messag
+	TablePool::erase(i);
+}
+
+void TableServer::serveTables() {}  // end serveTables
+
 TableServer::~TableServer() {}
--- serverapp/src/Module/TableServer.hpp
+++ serverapp/src/Module/TableServer.hpp
@@ -3,29 +3,50 @@
 #ifndef __TABLESERVER_HPP__
 #define __TABLESERVER_HPP__
 
-#include "GameServer.hpp"
 #include "include/Concurrent/Queue.hpp"
+#include "SlotPool.hpp"
+#include "RandomProvider.hpp"
 
+
+class Table;
 class PlayModule;
+class DataModule;
+class AuthModule;
+
 
 class TableServer {
 public:
-	TableServer(PlayModule& parent);
+	using Table_type = Table;
+	using TablePool = SlotPool<Table_type>;
 
-	void enqueueMatch(const GameServer::ConnectionId p1,
-					  const GameServer::ConnectionId p2);
-void run();
+public:
+	TableServer(PlayModule& parent, AuthModule* auth, DataModule* data,
+				GameServer::Database_type* db, GameServer::LogServer_type& ls);
+
+	void enqueueMatch(WSConnection p1, WSConnection p2);
+	void run();
 
 	~TableServer();
 
 private:
-	PlayModule& m_parent;
+	void createTable(const WSConnection p1, const WSConnection p2);
+	bool fillModel(Table_type* table, const WSConnection p1Connection, const WSConnection p2Connection);
+	void destroyTable(TablePool::index_type i);
 
-	using QueueMatch_type =
-		std::pair<GameServer::ConnectionId, GameServer::ConnectionId>;
+	void serveTables();
+
+private:
+	PlayModule& m_parent;
+		AuthModule* m_auth;
+	DataModule* m_data;
+	GameServer::Database_type* m_db;
+	GameServer::LogServer_type& logServer;
+	std::unique_ptr<RandomProvider> m_rand;
+
+
+	using QueueMatch_type = std::pair<WSConnection, WSConnection>;
 	Concurrent::Queue<QueueMatch_type> m_incomingMatches;
+
 };  // end class TableServer
 
 #endif
-
-
